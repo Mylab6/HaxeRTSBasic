bc7aa1dc6fbb93caa8972c14879d4695d32723ba
dreamihad@gmail.com <dreamihad@gmail.com>
2024-09-01 21:30:38 -0400

    2024-09-01 21:30:38

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "52";
+		app.meta["build"] = "53";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "HaxeRtsBasic";
 		app.meta["name"] = "HaxeRtsBasic";
--- source/managers/SpawnManager.hx
+++ source/managers/SpawnManager.hx
@@ -13,22 +13,28 @@ class SpawnManager
     private var blueBoxTimer:FlxTimer;
     private var greenBoxTimer:FlxTimer;
 	private var projectiles:FlxGroup;
+	private var redBoxSpawnRate:Float;
+	private var blueBoxSpawnRate:Float;
+	private var greenBoxSpawnRate:Float;
 
 	public function new(spawners:Array<SpawnerInfo>, projectiles:FlxGroup)
-
     {
         this.spawners = spawners;
 		this.projectiles = projectiles; 
         redBoxTimer = new FlxTimer();
         blueBoxTimer = new FlxTimer();
         greenBoxTimer = new FlxTimer();
+		redBoxSpawnRate = 2; // Default spawn rate for red box
+		blueBoxSpawnRate = 6; // Default spawn rate for blue box
+		greenBoxSpawnRate = 4; // Default spawn rate for green box
     }
 
     public function startSpawning():Void
     {
-        redBoxTimer.start(2, spawnRedBox, 0); // Spawn a red box every 2 seconds
-		blueBoxTimer.start(6, spawnBlueBox, 0); // Spawn a blue box every 3 seconds
-        greenBoxTimer.start(4, spawnGreenBox, 0); // Spawn a green box every 4 seconds
+		redBoxTimer.start(redBoxSpawnRate, spawnRedBox, 0); // Spawn a red box every redBoxSpawnRate seconds
+		blueBoxTimer.start(blueBoxSpawnRate, spawnBlueBox, 0); // Spawn a blue box every blueBoxSpawnRate seconds
+		greenBoxTimer.start(greenBoxSpawnRate, spawnGreenBox, 0); // Spawn a green box every greenBoxSpawnRate seconds
+		// blueBoxTimer.reset(1); // Reset the blue box timer to start spawning immediately
     }
 
     private function spawnRedBox(timer:FlxTimer):Void
@@ -49,7 +55,6 @@ class SpawnManager
         var blueBox = new BlueBox(x, y);
         info.enemyGroup.add(blueBox);
 		info.enemyEmitterGroup.add(blueBox.emitter);
-
     }
 
     private function spawnGreenBox(timer:FlxTimer):Void
@@ -60,6 +65,15 @@ class SpawnManager
 		var greenBox = new GreenBox(x, y, projectiles);
         info.enemyGroup.add(greenBox);
 		info.enemyEmitterGroup.add(greenBox.emitter);
+	}
 
+	public function updateSpawnRates(redBoxRate:Float, blueBoxRate:Float, greenBoxRate:Float):Void
+	{
+		redBoxSpawnRate = redBoxRate;
+		blueBoxSpawnRate = blueBoxRate;
+		greenBoxSpawnRate = greenBoxRate;
+		redBoxTimer.reset(redBoxSpawnRate);
+		blueBoxTimer.reset(blueBoxSpawnRate);
+		greenBoxTimer.reset(greenBoxSpawnRate);
     }
 }
